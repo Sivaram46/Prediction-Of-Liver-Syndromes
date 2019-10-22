f7b2ef6312ceee9343c68ed4c45f550408be1078
Saroopashree <saroopa25@gmail.com>
2019-10-22 10:53:06 +0530

    Add average value for NA entries in Albumin_and_Globulin_Ratio column in the dataset

--- src/main.R
+++ src/main.R
@@ -27,10 +27,39 @@ data.cleansing <- function() {
   }
 }
 
+get.average <- function(row1, x) {
+  age <- row1[1] %/% 10             # 1 corresponds to Age
+  age = age*10
+  print(paste("Age limit: ", age, "-", age+10))
+  average = 0
+  count = 0
+  for (y in 1:nrow(liver_patient)) {
+    if (y != x) {
+      row2 <- liver_patient[y,]
+      if (row2[1] >= age & row2[1] <= age+10 ) {
+        average = average + row2[10]
+        count = count + 1
+      }
+    }
+  }
+  average = average / count
+  return(average)
+}
+
 main <- function() {
   print(typeof(liver_patient))
   
-  data.cleansing()
+  # data.cleansing()
+  
+  for (x in 1:nrow(liver_patient)) {
+    row <- liver_patient[x,]
+    if (is.na(row[10])) {
+      average <- get.average(row, x)
+      liver_patient[x,][10] <- average
+      print(liver_patient[x,][10])
+    }
+  }
+  
   # print(head(liver_patient))
   print(summary(liver_patient))
   
