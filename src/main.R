c98da4f76e66bfbf844a82f10c925fd09f36ee7b
Saroopashree <saroopa25@gmail.com>
2019-10-22 21:24:55 +0530

    Create a new data frame liver.data

--- src/main.R
+++ src/main.R
@@ -1,31 +1,4 @@
-data.cleansing <- function() {
-  liver_patient$Gender <- as.integer(liver_patient$Gender)      # 1 - Female | 2 - Male
-  
-  for (x in 1:nrow(liver_patient)) {
-    row1 <- liver_patient[x,]
-    # print(row1[10])
-    if (is.na(row1[10])) {              # 10 corresponds to Albumin_and_Globulin_Ratio which has NA in dataset
-      age <- row1[1] %/% 10             # 1 corresponds to Age
-      age = age*10
-      print(paste("Age limit: ", age, "-", age+10))
-      average = 0
-      count = 0
-      for (y in 1:nrow(liver_patient)) {
-        if (y != x) {
-          row2 <- liver_patient[y,]
-          if (row2[1] >= age & row2[1] <= age+10 ) {
-            average = average + row2[10]
-            count = count + 1
-          }
-        }
-      }
-      average = average / count
-      # print(round(average, 1))
-      liver_patient[x,][10] <- round(average, 1)
-      print(liver_patient[x,][10])
-    }
-  }
-}
+# source("./src/data_visualization.R")
 
 get.average <- function(row1, x) {
   age <- row1[1] %/% 10             # 1 corresponds to Age
@@ -49,9 +22,7 @@ get.average <- function(row1, x) {
 main <- function() {
   print(typeof(liver_patient))
   
-  # data.cleansing()
-  
-  for (x in 1:nrow(liver_patient)) {
+  for (x in 1:nrow(liver_patient)) {            # Creating valid data value for NA entries
     row <- liver_patient[x,]
     if (is.na(row[10])) {
       average <- get.average(row, x)
@@ -61,21 +32,25 @@ main <- function() {
   }
   
   # print(head(liver_patient))
-  print(summary(liver_patient))
+  # print(summary(liver_patient))
   
   liver.data <- with(liver_patient, data.frame(Age,
-                                               Gender = as.integer(Gender),
-                                               TotBilirubin = Total_Bilirubin,
-                                               DirBilirubin = Direct_Bilirubin,
+                                               Gender = factor(Gender, levels=c("Female", "Male"), labels=c("Female", "Male")),
+                                               TotBrubin = Total_Bilirubin,
+                                               DirBrubin = Direct_Bilirubin,
                                                AlkPhosph = Alkaline_Phosphotase,
                                                AlamAmino = Alamine_Aminotransferase,
                                                AspAmino = Aspartate_Aminotransferase,
-                                               TotProteins=Total_Protiens,
+                                               TotProt=Total_Protiens,
                                                Albumin,
-                                               AlbuminGlobulinRatio = Albumin_and_Globulin_Ratio,
-                                               Result=Dataset))
+                                               AlbGlobRat = Albumin_and_Globulin_Ratio,
+                                               Result=factor(Dataset, levels=c(1, 2), labels=c("Yes", "No"))))
   
   print(typeof(liver.data))
+  print(summary(liver.data))
+  
+
+  # visualize(liver.data)
 }
 
 
